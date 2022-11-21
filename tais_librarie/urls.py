c302b7934857e3a85356900d4b725222c8863e78
KELVIN TICLLAHUANACO <sistemas.alfanevado@gmail.com>
2022-11-20 23:27:48 -0500

    templates

--- tais_librarie/urls.py
+++ tais_librarie/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from libros import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('libros/', views.helloworld)
     #path(''),
 ]
